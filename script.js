a6ffee889f9b7625a608c682da33630c65352a1c
kennytrbl <kennyzhang418@gmail.com>
2024-07-27 01:24:33 -0700

    local push

--- script.js
+++ script.js
@@ -1,62 +1,352 @@
 var songs = [
-    "eelfrHtmk68",
-    "D2sMg8mCHds",
-    "4iFP_wd6QU8",
-    "Vyo2m2CbdUs",
-    "Y93Jjvhz1jg",
-    "-7Uo2oKHGyU",
-    "QU9c0053UAU",
-    "wKyMIrBClYw",
-    "Z3W0jKcv1SU",
-    "29ycT6fA-Rs",
-    "UNo0TG9LwwI",
-    "SctIHP5dPyM",
-    "rCeM57e2BfU",
-    "8wpX6164uk4",
-    "3ze6drtwiE4",
-    "jOTfBlKSQYY",
-    "D8VEhcPeSlc",
-    "IRNBtocmQ58",
-    "kcelgrGY1h8",
-    "dZs_cLHfnNA",
-    "sk6WiGOmlso",
-    "hiUPJaHjuUk",
-    "v7bnOxV4jAc",
-    "Os_heh8vPfs",
-    "5_n6t9G2TUQ",
-    "mHNCM-YALSA",
-    "bNKXxwOQYB8",
-    "zZwFi86zIZM",
-    "HKBPmZbrsm0",
-    "w0sOLQXCGjE",
-    "V4tmJoX4NsI",
-    "Z4qVfCi_lUI",
-    "jRRCWFLAO6k",
-    "PS0qkO5qty0",
-    "zgxSYEFcePY",
-    "Vk5-c_v4gMU",
-    "iy9qZR_OGa0",
-    "lz46lUhrW5w",
-    "xfqBQ2XhBCg",
-    "6Q5xqNkCk7w",
-    "phuiiNCxRMg",
-    "Q3K0TOvTOno",
-    "nFYwcndNuOY",
-    "w2DOQLVpvLg",
+    {
+        videoId: "rCeM57e2BfU",
+        name: "And July (Feat. DEAN, DJ Friz)",
+        artist: "Heize",
+        album: "And July",
+        duration: "3:48",
+    },
+    {
+        videoId: "sk6WiGOmlso",
+        name: "Shut Up & Groove (Feat. DEAN)",
+        artist: "Heize",
+        album: "And July",
+        duration: "3:20",
+    },
+    {
+        videoId: "eelfrHtmk68",
+        name: "D (Half Moon) ft. Gaeko",
+        artist: "DEANTRBL",
+        album: "130 Mood: TRBL",
+        duration: "3:51",
+    },
+    {
+        videoId: "8wpX6164uk4",
+        name: "풀어(Pour Up) (ft. Zico)",
+        artist: "DEANTRBL",
+        album: "130 Mood: TRBL",
+        duration: "3:33",
+    },
+    {
+        videoId: "3ze6drtwiE4",
+        name: "Bonnie & Clyde",
+        artist: "DEANTRBL",
+        album: "130 Mood: TRBL",
+        duration: "3:44",
+    },
+    {
+        videoId: "D2sMg8mCHds",
+        name: "Come Over ft. Ye Rin Baek",
+        artist: "DEANTRBL",
+        album: "Limbo",
+        duration: "4:27",
+    },
+    {
+        videoId: "Z3W0jKcv1SU",
+        name: "하루살이 (dayfly) ft. 설리, Rad Museum",
+        artist: "DEANTRBL",
+        album: "하루살이",
+        duration: "4:17",
+    },
+    {
+        videoId: "hiUPJaHjuUk",
+        name: "Troll (Feat. DEAN)",
+        artist: "IU",
+        album: "IU 5th Album 'LILAC'",
+        duration: "3:09",
+    },
+    {
+        videoId: "v7bnOxV4jAc",
+        name: "LILAC",
+        artist: "IU",
+        album: "IU 5th Album 'LILAC'",
+        duration: "4:39",
+    },
+    {
+        videoId: "4iFP_wd6QU8",
+        name: "Square (2017)",
+        artist: "Yerin Baek",
+        album: "Every letter I sent you",
+        duration: "4:22",
+    },
+    {
+        videoId: "IRNBtocmQ58",
+        name: "Maybe It's Not Our Fault",
+        artist: "Yerin Baek",
+        album: "Our Love Is Great",
+        duration: "3:59",
+    },
+    {
+        videoId: "Vyo2m2CbdUs",
+        name: "Blue Flame",
+        artist: "LE SSERAFIM",
+        album: "Fearless",
+        duration: "3:21",
+    },
+    {
+        videoId: "Y93Jjvhz1jg",
+        name: "Why do u say (Feat. MOON, ASH ISLAND)",
+        artist: "Way Ched",
+        album: "Why do u say",
+        duration: "2:50",
+    },
+    {
+        videoId: "-7Uo2oKHGyU",
+        name: "Only U (Original Ver.)",
+        artist: "Moon Sujin",
+        album: "Only U",
+        duration: "3:47",
+    },
+    {
+        videoId: "29ycT6fA-Rs",
+        name: "Mayday (Feat. Joy)",
+        artist: "Crush",
+        album: "homemade 1",
+        duration: "3:20",
+    },
+    {
+        videoId: "QU9c0053UAU",
+        name: "Seven (feat. Latto)",
+        artist: "Jung Kook",
+        album: "Seven (feat. Latto)",
+        duration: "3:46",
+    },
+    {
+        videoId: "UNo0TG9LwwI",
+        name: "Standing Next to You",
+        artist: "Jung Kook",
+        album: "GOLDEN",
+        duration: "3:46",
+    },
+    {
+        videoId: "SctIHP5dPyM",
+        name: "DIE 4 YOU",
+        artist: "DEANTRBL",
+        album: "DIE 4 YOU",
+        duration: "3:15",
+    },
+    {
+        videoId: "D8VEhcPeSlc",
+        name: "Drama",
+        artist: "aespa",
+        album: "Drama - The 4th Mini Album",
+        duration: "3:47",
+    },
+    {
+        videoId: "Os_heh8vPfs",
+        name: "Spicy",
+        artist: "aespa",
+        album: "MY WORLD - The 3rd Mini Album",
+        duration: "3:24",
+    },
+    {
+        videoId: "5_n6t9G2TUQ",
+        name: "To.X",
+        artist: "TAEYEON",
+        album: "To.X - The 5th Mini Album",
+        duration: "2:57",
+    },
+    {
+        videoId: "zZwFi86zIZM",
+        name: "Yes or No (Feat. HUH YUNJIN of LE SSERAFIM, Crush)",
+        artist: "GroovyRoom",
+        album: "Yes or No",
+        duration: "3:02",
+    },
+    {
+        videoId: "HKBPmZbrsm0",
+        name: "Ego",
+        artist: "Crush",
+        album: "wonderego",
+        duration: "3:02",
+    },
+    {
+        videoId: "V4tmJoX4NsI",
+        name: "Monday Blues",
+        artist: "Crush",
+        album: "wonderego",
+        duration: "3:12",
+    },
+    {
+        videoId: "w0sOLQXCGjE",
+        name: "Chi-Ka (feat. DEAN)",
+        artist: "Tabber",
+        album: "Chi-Ka",
+        duration: "3:23",
+    },
+    {
+        videoId: "Z4qVfCi_lUI",
+        name: "Love You With All My Heart",
+        artist: "Crush",
+        album: "Queen of Tears (Original Television Soundtrack)",
+        duration: "4:04",
+    },
+    {
+        videoId: "jRRCWFLAO6k",
+        name: "Hold Me Back",
+        artist: "Heize",
+        album: "Queen of Tears (Original Television Soundtrack)",
+        duration: "3:52",
+    },
+    {
+        videoId: "PS0qkO5qty0",
+        name: "Rush Hour (Feat. j-hope of BTS)",
+        artist: "Crush",
+        album: "Rush Hour",
+        duration: "3:02",
+    },
+    {
+        videoId: "zgxSYEFcePY",
+        name: "Howlin' 404",
+        artist: "DEANTRBL",
+        album: "Howlin' 404",
+        duration: "3:47",
+    },
+    {
+        videoId: "Vk5-c_v4gMU",
+        name: "Magenetic",
+        artist: "ILLIT",
+        album: "SUPER REAL ME",
+        duration: "3:08",
+    },
+    {
+        videoId: "lz46lUhrW5w",
+        name: "Better (Feat. 쿠기)",
+        artist: "Moon Sujin",
+        album: "Better",
+        duration: "2:41",
+    },
+    {
+        videoId: "xfqBQ2XhBCg",
+        name: "SPOT! (feat. JENNIE)",
+        artist: "ZICO",
+        album: "SPOT!",
+        duration: "3:12",
+    },
+    {
+        videoId: "phuiiNCxRMg",
+        name: "Supernova",
+        artist: "aespa",
+        album: "Supernova",
+        duration: "3:13",
+    },
+    {
+        videoId: "nFYwcndNuOY",
+        name: "Armageddon",
+        artist: "aespa",
+        album: "Armageddon - The 1st Album",
+        duration: "3:32",
+    },
+    {
+        videoId: "w2DOQLVpvLg",
+        name: "Sun and Moon",
+        artist: "aespa",
+        album: "Hot Mess",
+        duration: "3:19",
+    },
+    {
+        videoId: "ahBKiNZFBNI",
+        name: "NewJeans - New Jeans (Cover) AI",
+        artist: "DEANTRBL",
+        album: "",
+        duration: "1:48",
+    },
+    {
+        videoId: "iGKqh1DiNqI",
+        name: "NewJeans - Ditto (A.I. cover)",
+        artist: "DEANTRBL",
+        album: "",
+        duration: "2:14",
+    },
+    {
+        videoId: "5tcBJCouOmE",
+        name: "NewJeans - Hype Boy (A.I. cover)",
+        artist: "DEANTRBL",
+        album: "",
+        duration: "2:47",
+    },
+    {
+        videoId: "e8ybbaxw2UM",
+        name: "NewJeans - Get Up (City Pop Remix)",
+        artist: "Matty Prasty",
+        album: "",
+        duration: "5:08",
+    },
+    {
+        videoId: "kTefaqHHLtI",
+        name: "Yerin Baek - Maybe It's Not Our Fault (A.I. cover)",
+        artist: "IU",
+        album: "",
+        duration: "3:53",
+    },
+    {
+        videoId: "6s-nDXnISQU",
+        name: "LE SSERAFIM - Perfect Night (City Pop Remix)",
+        artist: "Matt Prasty",
+        album: "",
+        duration: "4:32",
+    },
+    {
+        videoId: "3vbSOmZKKV8",
+        name: "TAEYEON - To. X (AI Cover)",
+        artist: "DEANTRBL",
+        album: "",
+        duration: "2:50",
+    },
+    {
+        videoId: "VUsDpXkTntc",
+        name: "TAEYEON - To. X (A.I. cover)",
+        artist: "Jung Kook",
+        album: "",
+        duration: "2:52",
+    },
+    {
+        videoId: "c12nDjyHN-8",
+        name: "ILLIT - Magnetic (Cover) AI",
+        artist: "DEANTRBL",
+        album: "",
+        duration: "2:40",
+    },
+    {
+        videoId: "wWcuUryeiwY",
+        name: "ILLIT - AI Yerin Baek 'Magnetic' (City Pop ver.)",
+        artist: "Yerin Baek",
+        album: "",
+        duration: "3:10",
+    },
 ];
-var container = document.querySelector(".container");
-if (container) {
-    songs.forEach(function (link) {
-        var iframe = document.createElement("iframe");
-        iframe.src = "https://www.youtube.com/embed/".concat(link);
-        iframe.height = "270";
-        iframe.width = "480";
-        iframe.allow =
-            "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-        iframe.allowFullscreen = true;
-        container.appendChild(iframe);
+var songListDiv = document.getElementById("song-list");
+if (songListDiv) {
+    songs.forEach(function (song, index) {
+        var songDiv = document.createElement("a");
+        songDiv.classList.add("song-row", "flex", "items-center", "justify-between", "p-4", "rounded", "hover:bg-gray-800", "transition-colors", "duration-300");
+        songDiv.href = "https://www.youtube.com/watch?v=".concat(song.videoId);
+        songDiv.target = "_blank";
+        songDiv.rel = "noopener noreferrer";
+        songDiv.innerHTML = "\n      <span class=\"songNumber w-10 text-center mr-4 text-white\">".concat(index + 1, "</span>\n      <img src=\"https://img.youtube.com/vi/").concat(song.videoId, "/mqdefault.jpg\" alt=\"").concat(song.name, "\">\n      <div class=\"flex-1 ml-4 mr-auto\">\n        <span class=\"block font-bold text-base text-white\">").concat(song.name, "</span>\n        <span class=\"block text-sm text-white\">").concat(song.artist, "</span>\n      </div>\n      <span class=\"songAlbum flex-1 text-sm text-white\">").concat(song.album, "</span>\n      <span class=\"songDuration w-16 text-right text-white\">").concat(song.duration, "</span>\n    ");
+        songListDiv.appendChild(songDiv);
     });
-}
-else {
-    console.error("Container element not found.");
+    var totalSongs = songs.length;
+    var totalDuration = songs.reduce(function (total, song) {
+        var _a = song.duration.split(":").map(Number), minutes = _a[0], seconds = _a[1];
+        return total + minutes * 60 + seconds;
+    }, 0);
+    var formatDuration = function (totalSeconds) {
+        var hours = Math.floor(totalSeconds / 3600);
+        var minutes = Math.floor((totalSeconds % 3600) / 60);
+        var duration = "";
+        if (hours > 0) {
+            duration += "".concat(hours, " hour").concat(hours > 1 ? "s" : "");
+        }
+        if (minutes > 0) {
+            if (duration)
+                duration += " ";
+            duration += "".concat(minutes, " minute").concat(minutes > 1 ? "s" : "");
+        }
+        return duration || "0 minutes";
+    };
+    document.getElementById("total-songs").textContent =
+        totalSongs.toString() + " songs";
+    document.getElementById("total-duration").textContent =
+        ", " + formatDuration(totalDuration);
 }
--- script.ts
+++ script.ts
@@ -1,64 +1,388 @@
-let songs: string[] = [
-  "eelfrHtmk68",
-  "D2sMg8mCHds",
-  "4iFP_wd6QU8",
-  "Vyo2m2CbdUs",
-  "Y93Jjvhz1jg",
-  "-7Uo2oKHGyU",
-  "QU9c0053UAU",
-  "wKyMIrBClYw",
-  "Z3W0jKcv1SU",
-  "29ycT6fA-Rs",
-  "UNo0TG9LwwI",
-  "SctIHP5dPyM",
-  "rCeM57e2BfU",
-  "8wpX6164uk4",
-  "3ze6drtwiE4",
-  "jOTfBlKSQYY",
-  "D8VEhcPeSlc",
-  "IRNBtocmQ58",
-  "kcelgrGY1h8",
-  "dZs_cLHfnNA",
-  "sk6WiGOmlso",
-  "hiUPJaHjuUk",
-  "v7bnOxV4jAc",
-  "Os_heh8vPfs",
-  "5_n6t9G2TUQ",
-  "mHNCM-YALSA",
-  "bNKXxwOQYB8",
-  "zZwFi86zIZM",
-  "HKBPmZbrsm0",
-  "w0sOLQXCGjE",
-  "V4tmJoX4NsI",
-  "Z4qVfCi_lUI",
-  "jRRCWFLAO6k",
-  "PS0qkO5qty0",
-  "zgxSYEFcePY",
-  "Vk5-c_v4gMU",
-  "iy9qZR_OGa0",
-  "lz46lUhrW5w",
-  "xfqBQ2XhBCg",
-  "6Q5xqNkCk7w",
-  "phuiiNCxRMg",
-  "Q3K0TOvTOno",
-  "nFYwcndNuOY",
-  "w2DOQLVpvLg",
+interface Song {
+  videoId: string;
+  name: string;
+  artist: string;
+  album: string;
+  duration: string;
+}
+
+const songs: Song[] = [
+  {
+    videoId: "rCeM57e2BfU",
+    name: "And July (Feat. DEAN, DJ Friz)",
+    artist: "Heize",
+    album: "And July",
+    duration: "3:48",
+  },
+  {
+    videoId: "sk6WiGOmlso",
+    name: "Shut Up & Groove (Feat. DEAN)",
+    artist: "Heize",
+    album: "And July",
+    duration: "3:20",
+  },
+  {
+    videoId: "eelfrHtmk68",
+    name: "D (Half Moon) ft. Gaeko",
+    artist: "DEANTRBL",
+    album: "130 Mood: TRBL",
+    duration: "3:51",
+  },
+  {
+    videoId: "8wpX6164uk4",
+    name: "풀어(Pour Up) (ft. Zico)",
+    artist: "DEANTRBL",
+    album: "130 Mood: TRBL",
+    duration: "3:33",
+  },
+  {
+    videoId: "3ze6drtwiE4",
+    name: "Bonnie & Clyde",
+    artist: "DEANTRBL",
+    album: "130 Mood: TRBL",
+    duration: "3:44",
+  },
+  {
+    videoId: "D2sMg8mCHds",
+    name: "Come Over ft. Ye Rin Baek",
+    artist: "DEANTRBL",
+    album: "Limbo",
+    duration: "4:27",
+  },
+  {
+    videoId: "Z3W0jKcv1SU",
+    name: "하루살이 (dayfly) ft. 설리, Rad Museum",
+    artist: "DEANTRBL",
+    album: "하루살이",
+    duration: "4:17",
+  },
+  {
+    videoId: "hiUPJaHjuUk",
+    name: "Troll (Feat. DEAN)",
+    artist: "IU",
+    album: "IU 5th Album 'LILAC'",
+    duration: "3:09",
+  },
+  {
+    videoId: "v7bnOxV4jAc",
+    name: "LILAC",
+    artist: "IU",
+    album: "IU 5th Album 'LILAC'",
+    duration: "4:39",
+  },
+  {
+    videoId: "4iFP_wd6QU8",
+    name: "Square (2017)",
+    artist: "Yerin Baek",
+    album: "Every letter I sent you",
+    duration: "4:22",
+  },
+  {
+    videoId: "IRNBtocmQ58",
+    name: "Maybe It's Not Our Fault",
+    artist: "Yerin Baek",
+    album: "Our Love Is Great",
+    duration: "3:59",
+  },
+  {
+    videoId: "Vyo2m2CbdUs",
+    name: "Blue Flame",
+    artist: "LE SSERAFIM",
+    album: "Fearless",
+    duration: "3:21",
+  },
+  {
+    videoId: "Y93Jjvhz1jg",
+    name: "Why do u say (Feat. MOON, ASH ISLAND)",
+    artist: "Way Ched",
+    album: "Why do u say",
+    duration: "2:50",
+  },
+  {
+    videoId: "-7Uo2oKHGyU",
+    name: "Only U (Original Ver.)",
+    artist: "Moon Sujin",
+    album: "Only U",
+    duration: "3:47",
+  },
+  {
+    videoId: "29ycT6fA-Rs",
+    name: "Mayday (Feat. Joy)",
+    artist: "Crush",
+    album: "homemade 1",
+    duration: "3:20",
+  },
+  {
+    videoId: "QU9c0053UAU",
+    name: "Seven (feat. Latto)",
+    artist: "Jung Kook",
+    album: "Seven (feat. Latto)",
+    duration: "3:46",
+  },
+  {
+    videoId: "UNo0TG9LwwI",
+    name: "Standing Next to You",
+    artist: "Jung Kook",
+    album: "GOLDEN",
+    duration: "3:46",
+  },
+  {
+    videoId: "SctIHP5dPyM",
+    name: "DIE 4 YOU",
+    artist: "DEANTRBL",
+    album: "DIE 4 YOU",
+    duration: "3:15",
+  },
+  {
+    videoId: "D8VEhcPeSlc",
+    name: "Drama",
+    artist: "aespa",
+    album: "Drama - The 4th Mini Album",
+    duration: "3:47",
+  },
+  {
+    videoId: "Os_heh8vPfs",
+    name: "Spicy",
+    artist: "aespa",
+    album: "MY WORLD - The 3rd Mini Album",
+    duration: "3:24",
+  },
+  {
+    videoId: "5_n6t9G2TUQ",
+    name: "To.X",
+    artist: "TAEYEON",
+    album: "To.X - The 5th Mini Album",
+    duration: "2:57",
+  },
+  {
+    videoId: "zZwFi86zIZM",
+    name: "Yes or No (Feat. HUH YUNJIN of LE SSERAFIM, Crush)",
+    artist: "GroovyRoom",
+    album: "Yes or No",
+    duration: "3:02",
+  },
+  {
+    videoId: "HKBPmZbrsm0",
+    name: "Ego",
+    artist: "Crush",
+    album: "wonderego",
+    duration: "3:02",
+  },
+  {
+    videoId: "V4tmJoX4NsI",
+    name: "Monday Blues",
+    artist: "Crush",
+    album: "wonderego",
+    duration: "3:12",
+  },
+  {
+    videoId: "w0sOLQXCGjE",
+    name: "Chi-Ka (feat. DEAN)",
+    artist: "Tabber",
+    album: "Chi-Ka",
+    duration: "3:23",
+  },
+  {
+    videoId: "Z4qVfCi_lUI",
+    name: "Love You With All My Heart",
+    artist: "Crush",
+    album: "Queen of Tears (Original Television Soundtrack)",
+    duration: "4:04",
+  },
+  {
+    videoId: "jRRCWFLAO6k",
+    name: "Hold Me Back",
+    artist: "Heize",
+    album: "Queen of Tears (Original Television Soundtrack)",
+    duration: "3:52",
+  },
+  {
+    videoId: "PS0qkO5qty0",
+    name: "Rush Hour (Feat. j-hope of BTS)",
+    artist: "Crush",
+    album: "Rush Hour",
+    duration: "3:02",
+  },
+  {
+    videoId: "zgxSYEFcePY",
+    name: "Howlin' 404",
+    artist: "DEANTRBL",
+    album: "Howlin' 404",
+    duration: "3:47",
+  },
+  {
+    videoId: "Vk5-c_v4gMU",
+    name: "Magenetic",
+    artist: "ILLIT",
+    album: "SUPER REAL ME",
+    duration: "3:08",
+  },
+  {
+    videoId: "lz46lUhrW5w",
+    name: "Better (Feat. 쿠기)",
+    artist: "Moon Sujin",
+    album: "Better",
+    duration: "2:41",
+  },
+  {
+    videoId: "xfqBQ2XhBCg",
+    name: "SPOT! (feat. JENNIE)",
+    artist: "ZICO",
+    album: "SPOT!",
+    duration: "3:12",
+  },
+  {
+    videoId: "phuiiNCxRMg",
+    name: "Supernova",
+    artist: "aespa",
+    album: "Supernova",
+    duration: "3:13",
+  },
+  {
+    videoId: "nFYwcndNuOY",
+    name: "Armageddon",
+    artist: "aespa",
+    album: "Armageddon - The 1st Album",
+    duration: "3:32",
+  },
+  {
+    videoId: "w2DOQLVpvLg",
+    name: "Sun and Moon",
+    artist: "aespa",
+    album: "Hot Mess",
+    duration: "3:19",
+  },
+  {
+    videoId: "ahBKiNZFBNI",
+    name: "NewJeans - New Jeans (Cover) AI",
+    artist: "DEANTRBL",
+    album: "",
+    duration: "1:48",
+  },
+  {
+    videoId: "iGKqh1DiNqI",
+    name: "NewJeans - Ditto (A.I. cover)",
+    artist: "DEANTRBL",
+    album: "",
+    duration: "2:14",
+  },
+  {
+    videoId: "5tcBJCouOmE",
+    name: "NewJeans - Hype Boy (A.I. cover)",
+    artist: "DEANTRBL",
+    album: "",
+    duration: "2:47",
+  },
+  {
+    videoId: "e8ybbaxw2UM",
+    name: "NewJeans - Get Up (City Pop Remix)",
+    artist: "Matty Prasty",
+    album: "",
+    duration: "5:08",
+  },
+  {
+    videoId: "kTefaqHHLtI",
+    name: "Yerin Baek - Maybe It's Not Our Fault (A.I. cover)",
+    artist: "IU",
+    album: "",
+    duration: "3:53",
+  },
+  {
+    videoId: "6s-nDXnISQU",
+    name: "LE SSERAFIM - Perfect Night (City Pop Remix)",
+    artist: "Matt Prasty",
+    album: "",
+    duration: "4:32",
+  },
+  {
+    videoId: "3vbSOmZKKV8",
+    name: "TAEYEON - To. X (AI Cover)",
+    artist: "DEANTRBL",
+    album: "",
+    duration: "2:50",
+  },
+  {
+    videoId: "VUsDpXkTntc",
+    name: "TAEYEON - To. X (A.I. cover)",
+    artist: "Jung Kook",
+    album: "",
+    duration: "2:52",
+  },
+  {
+    videoId: "c12nDjyHN-8",
+    name: "ILLIT - Magnetic (Cover) AI",
+    artist: "DEANTRBL",
+    album: "",
+    duration: "2:40",
+  },
+  {
+    videoId: "wWcuUryeiwY",
+    name: "ILLIT - AI Yerin Baek 'Magnetic' (City Pop ver.)",
+    artist: "Yerin Baek",
+    album: "",
+    duration: "3:10",
+  },
 ];
+const songListDiv = document.getElementById("song-list");
 
-const container = document.querySelector(".container");
-
-if (container) {
-  songs.forEach(function (link) {
-    let iframe = document.createElement("iframe");
-    iframe.src = `https://www.youtube.com/embed/${link}`;
-    iframe.height = "270";
-    iframe.width = "480";
-    iframe.allow =
-      "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-    iframe.allowFullscreen = true;
-
-    container.appendChild(iframe);
+if (songListDiv) {
+  songs.forEach((song, index) => {
+    const songDiv = document.createElement("a");
+    songDiv.classList.add(
+      "song-row",
+      "flex",
+      "items-center",
+      "justify-between",
+      "p-4",
+      "rounded",
+      "hover:bg-gray-800",
+      "transition-colors",
+      "duration-300"
+    );
+    songDiv.href = `https://www.youtube.com/watch?v=${song.videoId}`;
+    songDiv.target = "_blank";
+    songDiv.rel = "noopener noreferrer";
+    songDiv.innerHTML = `
+      <span class="songNumber w-10 text-center mr-4 text-white">${
+        index + 1
+      }</span>
+      <img src="https://img.youtube.com/vi/${
+        song.videoId
+      }/mqdefault.jpg" alt="${song.name}">
+      <div class="flex-1 ml-4 mr-auto">
+        <span class="block font-bold text-base text-white">${song.name}</span>
+        <span class="block text-sm text-white">${song.artist}</span>
+      </div>
+      <span class="songAlbum flex-1 text-sm text-white">${song.album}</span>
+      <span class="songDuration w-16 text-right text-white">${
+        song.duration
+      }</span>
+    `;
+    songListDiv.appendChild(songDiv);
   });
-} else {
-  console.error("Container element not found.");
+
+  const totalSongs: number = songs.length;
+  const totalDuration: number = songs.reduce((total, song) => {
+    const [minutes, seconds] = song.duration.split(":").map(Number);
+    return total + minutes * 60 + seconds;
+  }, 0);
+
+  const formatDuration = (totalSeconds: number): string => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    let duration = "";
+    if (hours > 0) {
+      duration += `${hours} hour${hours > 1 ? "s" : ""}`;
+    }
+    if (minutes > 0) {
+      if (duration) duration += " ";
+      duration += `${minutes} minute${minutes > 1 ? "s" : ""}`;
+    }
+    return duration || "0 minutes";
+  };
+
+  document.getElementById("total-songs")!.textContent =
+    totalSongs.toString() + " songs";
+  document.getElementById("total-duration")!.textContent =
+    ", " + formatDuration(totalDuration);
 }
